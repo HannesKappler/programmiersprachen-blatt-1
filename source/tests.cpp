282fcac5c4d5c53446ae863c98305f8939747c17
HannesKappler <hakappler@gmail.com>
2016-04-25 14:04:23 +0200

    Added files via upload

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+
+int gcd (int a, int b)
+{
+	return 1;
+}
+
+TEST_CASE("describe_gcd", "[gcd]")
+{
+	REQUIRE(gcd(2,4) == 2);
+	REQUIRE(gcd(6,9) == 3);
+	REQUIRE(gcd(3,7) == 1);
+}
+
 int main(int argc, char* argv[])
 {
-  return Catch::Session().run(argc, argv);
+  	return Catch::Session().run(argc, argv);
 }
